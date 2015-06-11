d3a19b0e64d15b5f43af7f7418115afccbf3eea6
Benedict Romp <b.romp@meeva.de>
2015-06-11 13:15:20 +0200

    Network Navi: Icons + Network Logo

--- app/views/network_navi.blade.php
+++ app/views/network_navi.blade.php
@@ -1,3 +1,7 @@
 <div class="network_navi">
-	<div class="network_logo">Logo</div>
+	<div class="account_part">
+		<div class="login_btn nw_navi_el hovered"><i class="fa fa-sign-in"></i> Einloggen</div>
+		<div class="login_btn nw_navi_el hovered"><i class="fa fa-user"></i> Registrieren</div>
+	</div>
+	<div class="network_logo">FLASHIGNITE Network</div>
 </div>
