16a3dee8199bb68dda7d380bd444546639180bcc
Benedict Romp <benedict.romp@googlemail.com>
2015-06-21 18:13:19 +0200

    FIServer Klasse erfolgreich eingebunden: Verbindung zum WS-Server klappt nun

--- app/controllers/IndexController.php
+++ app/controllers/IndexController.php
@@ -2,6 +2,7 @@
 
 class IndexController extends \BaseController {
 	public function index(){
+        //FIServer::send("teeest"); // Test-Nachricht an den FI-WS-Server
 		return View::make("index");
 	}
 }
