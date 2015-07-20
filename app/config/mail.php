8de0b3c0587f52134c2379312e00798c7d884522
Mark Tomicki <marktomicki@gmail.com>
2015-07-20 16:33:39 +0200

    smtp

--- app/config/mail.php
+++ app/config/mail.php
@@ -80,7 +80,7 @@ return array(
 	|
 	*/
 
-	'username' => "teamrankedcom@gmail.com",
+	'username' => 'teamrankedcom@gmail.com',
 
 	/*
 	|--------------------------------------------------------------------------
@@ -93,7 +93,7 @@ return array(
 	|
 	*/
 
-	'password' => "gutenmorgenteam!",
+	'password' => 'gutenmorgenteam!',
 
 	/*
 	|--------------------------------------------------------------------------
