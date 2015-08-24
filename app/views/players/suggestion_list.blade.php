81f4cc6d5d7d97cb701f9c4b55ae101299d677d4
Benedict Romp <benedict.romp@googlemail.com>
2015-08-24 15:20:26 +0200

    Player-Suggestions responsive

--- app/views/players/suggestion_list.blade.php
+++ app/views/players/suggestion_list.blade.php
@@ -1,7 +1,84 @@
 @if($player_list AND $player_list->count() > 0)
     @foreach($player_list as $player)
         <div class="strip_all_tour_list player_searchbox wow fadeIn" data-wow-delay="0.1s">
-            <div class="row">
+            <div class="row small_version">
+                <div class="col-xs-3" style="text-align: right;">
+                     @if($player->solo_tier AND trim($player->solo_tier) != "" AND trim($player->solo_tier) != "none")
+                        <?php
+                        $pl_division = "1";
+                        if($player->solo_division == "II"){ $pl_division = "2"; }
+                        elseif($player->solo_division == "III"){ $pl_division = "3"; }
+                        elseif($player->solo_division == "IV"){ $pl_division = "4"; }
+                        elseif($player->solo_division == "V"){ $pl_division = "5"; }
+                        ?>
+                        <br/>
+                        <img style="width: 100%;" src="/img/leagues/{{ trim(strtolower($player->solo_tier)) }}_{{ trim($pl_division) }}.png" class="tooltips" title="{{ trim(ucfirst(strtolower($player->solo_tier))) }} {{ trim($pl_division) }}">
+                        <div style="padding-top: 15px;text-align: center;">{{ $player->solo_tier }} {{ $player->solo_division }}</div>
+                    @else
+                        <img style="width: 100%;" src="/img/leagues/0_5.png" class="tooltip" title="Unranked">
+                        <div style="padding-top: 15px;text-align: center;">Unranked</div>
+                    @endif
+                </div>
+
+                <div class="col-xs-9" style="text-align: left;">
+                    @if(Auth::check())
+                        <h3><a href="/summoner/{{ trim($player->region) }}/{{ trim($player->name) }}"><strong>{{ $player->name }}</strong></a></h3>
+                    @else
+                        <div style="margin-top: 20px;margin-bottom: 10px;height: 22px;">
+                            <a href="/login">Login to see detailled summoner information</a>
+                        </div>
+                    @endif
+
+                    <table class="list_info_table">
+                        <tr>
+                            <td class="title">Roles:</td>
+                            <td>
+                                @if($player->search_top == 1)
+                                    <img src="/img/roles/tank.jpg" class="img-circle" width="35" />
+                                    <?php $no_roles_open = false; ?>
+                                @endif
+                                @if($player->search_jungle == 1)
+                                    <img src="/img/roles/fighter.jpg" class="img-circle" width="35" />
+                                    <?php $no_roles_open = false; ?>
+                                @endif
+                                @if($player->search_mid == 1)
+                                    <img src="/img/roles/mage.jpg" class="img-circle" width="35" />
+                                    <?php $no_roles_open = false; ?>
+                                @endif
+                                @if($player->search_adc == 1)
+                                    <img src="/img/roles/marksman.jpg" class="img-circle" width="35" />
+                                    <?php $no_roles_open = false; ?>
+                                @endif
+                                @if($player->search_support == 1)
+                                    <img src="/img/roles/support.jpg" class="img-circle" width="35" />
+                                    <?php $no_roles_open = false; ?>
+                                @endif
+                            </td>
+                        </tr>
+                        @if($player->main_lang != "" || $player->sec_lang != "")
+                        <tr>
+                            <td class="title">Languages:</td>
+                            <td>
+                                @if($player->main_lang != "")
+                                    <img src="/img/flags/{{ $player->main_lang }}.png" class="img-circle" width="35" />
+                                @endif
+                                @if($player->sec_lang != "")
+                                    <img src="/img/flags/{{ $player->sec_lang }}.png" class="img-circle" width="35" />
+                                @endif
+                            </td>
+                        </tr>
+                        @endif
+                        <tr>
+                            <td class="title">Region:</td>
+                            <td>
+                                <span style="float: right;padding-right: 10px;"><a href="/summoner/{{ trim($player->region) }}/{{ trim($player->name) }}">more</a></span>
+                                {{ strtoupper($player->region) }}
+                            </td>
+                        </tr>
+                    </table>
+                </div>
+            </div>
+            <div class="row desktop_version">
                 <div class="col-lg-3 col-md-3 col-sm-3">
                     <div class="center">
                         <div style="text-align: center">
@@ -24,7 +101,7 @@
                     </div>
                 </div>
                 <div class="clearfix visible-xs-block"></div>
-                <div class="col-lg-9 col-md-9 col-sm-9">
+                <div class="col-lg-9 col-md-9 col-sm-9 no_small">
                     <div class="">
                         @if(Auth::check())
                             <h3><a href="/summoner/{{ trim($player->region) }}/{{ trim($player->name) }}"><strong>{{ $player->name }}</strong></a></h3>
