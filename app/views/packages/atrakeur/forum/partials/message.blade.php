f48406f57ae91e87d408669557bc3c0afd184f79
Mark Tomicki <marktomicki@gmail.com>
2015-08-26 12:47:22 +0200

    bugfix

--- app/views/packages/atrakeur/forum/partials/message.blade.php
+++ app/views/packages/atrakeur/forum/partials/message.blade.php
@@ -10,9 +10,11 @@
 				<a href="/summoner/{{ $message->author->region }}/{{{ Helpers::get_summoner($message->author->summoner_id, $message->author->region)->name }}}">
 					{{{ Helpers::get_summoner($message->author->summoner_id, $message->author->region)->name }}}
 				</a><br/>
-				@if($message->author->hasRole("admin"))
+				/*
+				@if((Helpers::getUser($message->author->id))->hasRole("admin"))
 					<i>Administrator</i>
 				@endif
+				*/
 			</td>
 			<td class="author_message">
 				{{ nl2br(e($message->data)) }}
